788962226a32db848f047a5ef42f9b1d1bdc3777
ihfazhillah <mihfazhillah@gmail.com>
2022-12-03 13:14:51 +0700

    add ordering

--- warnain/printable_books/views.py
+++ warnain/printable_books/views.py
@@ -1,5 +1,6 @@
 import cups
 from django.conf import settings
+from django.db import models
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.request import Request
@@ -13,6 +14,28 @@ class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     search_fields = ["title"]
+
+    def get_queryset(self):
+        qs = super().get_queryset().annotate(
+            access_count=models.Count("access"),
+            latest_access=models.Subquery(
+                CategoryAccess.objects.filter(
+                    category_id=models.OuterRef("id")
+                ).order_by("-created").values("created")[:1]
+            )
+        )
+
+        sort = self.request.GET.get("sort_by", "title")
+        order_by = "-id"
+
+        if sort == "title":
+            order_by = "title"
+        elif sort == "freq":
+            order_by = "-access_count"
+        elif sort == "access":
+            order_by = "-latest_access"
+
+        return qs.order_by(order_by)
 
 
 @api_view(["GET"])
